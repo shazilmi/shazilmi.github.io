4157732f95a68ccb56440635cc56f32f1f04ede3
shazilmi <shazilmiqbal@protonmail.com>
2025-06-19 10:57:23 +0530

    Second try on router

--- my-component.js
+++ my-component.js
@@ -4,5 +4,6 @@ export default {
         const count = ref(0);
         return { count }
     },
-    template: `<div>Count is: {{ count }}</div>`
+    template: `<div>This is from Vue.js
+    <p><RouterLink to = "/home">This is a link to home. </RouterLink></p></div>`
 }
